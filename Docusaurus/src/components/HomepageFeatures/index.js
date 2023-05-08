24c6a4160afce9d87256670d58f862996e73e9a1
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2023-05-08 14:36:34 +0700

    modified:   src/components/HomepageFeatures/index.js new file:   static/img/C-neuman.jpg

--- Docusaurus/src/components/HomepageFeatures/index.js
+++ Docusaurus/src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Ken Arok',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
          Ini baru lumayan, buat foya-foya, kalau di nirwana.
@@ -23,7 +23,7 @@ const FeatureList = [
   },
   {
     title: 'TJ Maxx',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
          Ttttttt... TJ Maxx.
