6b0f6b261488c0a5fca6c073504464812e43cc97
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2023-05-31 13:19:01 +0700

    new file:   src/components/Highlight2.js

--- Docusaurus/docusaurus.config.js
+++ Docusaurus/docusaurus.config.js
@@ -114,9 +114,9 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} BinKadal, Sendirian Berhad --- Revision: 23c - 30 May 2023`,
+        copyright: `Copyright © ${new Date().getFullYear()} BinKadal, Sendirian Berhad --- Revision: 24 - 31 May 2023`,
         // ZCZC
-        // REV23: Tue 30 May 2023 12:00
+        // REV24: Wed 31 May 2023 13:00
         // REV19: Fri 26 May 2023 10:00
         // REV09: Thu 25 May 2023 07:00
         // REV05: Thu 11 May 2023 03:00
